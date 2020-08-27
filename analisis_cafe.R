d406c8d5b1de38d8f279cb14f2b289121165d844
Florencia D'Andrea <florencia.dandrea@gmail.com>
2020-08-27 19:15:36 -0300

    Update analisis_cafe.R

--- analisis_cafe.R
+++ analisis_cafe.R
@@ -7,7 +7,6 @@ coffee_data %>%
   pivot_longer(cols=aroma:moisture,
                names_to="caracteristicas_cafe",
                values_to="value") %>%
-  
   ggplot(aes(value, total_cup_points))+
   geom_smooth(method= "gam", span=0.3, color='purple')+
   facet_wrap(~caracteristicas_cafe, scale="free_y")
